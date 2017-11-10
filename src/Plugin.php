fc59b30160eb174569505ae43e9999afca54484b
Dan Smith <dev@dotherightthing.co.nz>
2017-11-10 14:05:32 +1300

    Roll tgm-plugin-activation.php into plugin - #17

--- src/Plugin.php
+++ src/Plugin.php
@@ -128,9 +128,168 @@ if ( !class_exists( 'Plugin' ) ) {
       add_action( 'wp_enqueue_scripts',       [$this, 'render_css_frontend'] );
       add_action( 'wp_enqueue_scripts',       [$this, 'render_js_frontend'] );
       add_action( 'admin_enqueue_scripts',    [$this, 'render_js_backend'] );
+      add_action( 'tgmpa_register',           [$this, 'register_required_plugins'] );
     
       // call the server side PHP function through admin-ajax.php.
       add_action( 'wp_ajax_refresh_api_data',  [$this, 'refresh_api_data'] );
+    }
+
+    /**
+     * TGM Plugin Activation Configuration.
+     * Registers the required plugins for this theme.
+     * This function is hooked into `tgmpa_register`,
+     * which is fired on the WP `init` action on priority 10.
+     *
+     * @version     2.6.1 for WPPlugin
+     * @author      Thomas Griffin, Gary Jones, Juliette Reinders Folmer
+     * @copyright   Copyright (c) 2011, Thomas Griffin
+     * @license     http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
+     * @link        https://github.com/TGMPA/TGM-Plugin-Activation
+     *
+     * @since       1.0.0
+     * @see         http://tgmpluginactivation.com/configuration/
+     */
+    public function register_required_plugins() {
+
+      /**
+       * Include the TGM_Plugin_Activation class.
+       *
+       * Parent Theme:
+       * require_once get_template_directory() . '/path/to/class-tgm-plugin-activation.php';
+       *
+       * Child Theme:
+       * require_once get_stylesheet_directory() . '/path/to/class-tgm-plugin-activation.php';
+       *
+       * Plugin:
+       * require_once dirname( __FILE__ ) . '/path/to/class-tgm-plugin-activation.php';
+       */
+      require_once( $this->get_path() . 'vendor/tgmpa/tgm-plugin-activation/class-tgm-plugin-activation.php');
+
+      /*
+       * Array of plugin arrays. Required keys are name and slug.
+       * If the source is NOT from the .org repo, then source is also required.
+       */
+      $plugins = array(
+
+          array(
+              'name'          => 'WordPress Admin Style',
+              'slug'          => 'wordpress-admin-style',
+              'source'        => 'https://github.com/bueltge/wordpress-admin-style/archive/master.zip',
+              'external_url'  => 'https://github.com/bueltge/wordpress-admin-style',
+              'required'      => false
+          )
+
+        /*
+          array(
+            'name'          => 'Advanced Custom Fields',
+            'slug'          => 'advanced-custom-fields',
+            'required'      => true,
+            'is_callable'   => array( 'acf_field_google_map', 'render_field' )
+          ),
+          */
+
+      );
+
+      /*
+       * Array of configuration settings. Amend each line as needed.
+       *
+       * TGMPA will start providing localized text strings soon. If you already have translations of our standard
+       * strings available, please help us make TGMPA even better by giving us access to these translations or by
+       * sending in a pull-request with .po file(s) with the translations.
+       *
+       * Only uncomment the strings in the config array if you want to customize the strings.
+       */
+      $config = array(
+        'id'           => $this->get_slug(),        // Unique ID for hashing notices for multiple instances of TGMPA.
+        'default_path' => '',                       // Default absolute path to bundled plugins.
+        'menu'         => 'tgmpa-install-plugins',  // Menu slug.
+        'parent_slug'  => 'plugins.php',            // Parent menu slug.
+        'capability'   => 'manage_options',         // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+        'has_notices'  => true,                     // Show admin notices or not.
+        'dismissable'  => true,                     // If false, a user cannot dismiss the nag message.
+        'dismiss_msg'  => '',                       // If 'dismissable' is false, this message will be output at top of nag.
+        'is_automatic' => false,                    // Automatically activate plugins after installation or not.
+        'message'      => '',                       // Message to output right before the plugins table.
+
+        /*
+        'strings'      => array(
+          'page_title'                      => __( 'Install Required Plugins', 'text-domain' ),
+          'menu_title'                      => __( 'Install Plugins', 'text-domain' ),
+          /* translators: %s: plugin name. * /
+          'installing'                      => __( 'Installing Plugin: %s', 'text-domain' ),
+          /* translators: %s: plugin name. * /
+          'updating'                        => __( 'Updating Plugin: %s', 'text-domain' ),
+          'oops'                            => __( 'Something went wrong with the plugin API.', 'text-domain' ),
+          'notice_can_install_required'     => _n_noop(
+            /* translators: 1: plugin name(s). * /
+            'This theme requires the following plugin: %1$s.',
+            'This theme requires the following plugins: %1$s.',
+            'text-domain'
+          ),
+          'notice_can_install_recommended'  => _n_noop(
+            /* translators: 1: plugin name(s). * /
+            'This theme recommends the following plugin: %1$s.',
+            'This theme recommends the following plugins: %1$s.',
+            'text-domain'
+          ),
+          'notice_ask_to_update'            => _n_noop(
+            /* translators: 1: plugin name(s). * /
+            'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
+            'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
+            'text-domain'
+          ),
+          'notice_ask_to_update_maybe'      => _n_noop(
+            /* translators: 1: plugin name(s). * /
+            'There is an update available for: %1$s.',
+            'There are updates available for the following plugins: %1$s.',
+            'text-domain'
+          ),
+          'notice_can_activate_required'    => _n_noop(
+            /* translators: 1: plugin name(s). * /
+            'The following required plugin is currently inactive: %1$s.',
+            'The following required plugins are currently inactive: %1$s.',
+            'text-domain'
+          ),
+          'notice_can_activate_recommended' => _n_noop(
+            /* translators: 1: plugin name(s). * /
+            'The following recommended plugin is currently inactive: %1$s.',
+            'The following recommended plugins are currently inactive: %1$s.',
+            'text-domain'
+          ),
+          'install_link'                    => _n_noop(
+            'Begin installing plugin',
+            'Begin installing plugins',
+            'text-domain'
+          ),
+          'update_link'             => _n_noop(
+            'Begin updating plugin',
+            'Begin updating plugins',
+            'text-domain'
+          ),
+          'activate_link'                   => _n_noop(
+            'Begin activating plugin',
+            'Begin activating plugins',
+            'text-domain'
+          ),
+          'return'                          => __( 'Return to Required Plugins Installer', 'text-domain' ),
+          'plugin_activated'                => __( 'Plugin activated successfully.', 'text-domain' ),
+          'activated_successfully'          => __( 'The following plugin was activated successfully:', 'text-domain' ),
+          /* translators: 1: plugin name. * /
+          'plugin_already_active'           => __( 'No action taken. Plugin %1$s was already active.', 'text-domain' ),
+          /* translators: 1: plugin name. * /
+          'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'text-domain' ),
+          /* translators: 1: dashboard link. * /
+          'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'text-domain' ),
+          'dismiss'                         => __( 'Dismiss this notice', 'text-domain' ),
+          'notice_cannot_install_activate'  => __( 'There are one or more required or recommended plugins to install, update or activate.', 'text-domain' ),
+          'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'text-domain' ),
+
+          'nag_type'                        => '', // Determines admin notice type - can only be one of the typical WP notice classes, such as 'updated', 'update-nag', 'notice-warning', 'notice-info' or 'error'. Some of which may not work as expected in older WP versions.
+        ),
+        */
+      );
+
+      tgmpa( $plugins, $config );
     }
 
     /**
