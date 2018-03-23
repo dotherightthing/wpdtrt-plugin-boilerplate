7c39975d7cfc0921a60326e96a736223df917595
Dan Smith <dev@dotherightthing.co.nz>
2018-03-23 13:05:02 +1300

    Bump version

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@
  *    gulp dist --gulpfile ./vendor/dotherightthing/wpdtrt-plugin/gulpfile.js --cwd ./
  *
  * @package     WPPlugin
- * @version     1.2.13
+ * @version     1.2.14
  */
 
 /* jshint node: true */
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * @see http://phpenthusiast.com/blog/how-to-autoload-with-composer
  *
  * @package   	WPPlugin
- * @version     1.2.13
+ * @version     1.2.14
  */
 
   /**
