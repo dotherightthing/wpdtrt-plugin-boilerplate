36f1fd84008413c5a3488ebe3f90d34c97e48d97
Dan Smith <dev@dotherightthing.co.nz>
2018-04-17 16:43:52 +1200

    Bump version to update Private Packagist copy for more accurate composer update

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@
  *    gulp dist --gulpfile ./vendor/dotherightthing/wpdtrt-plugin/gulpfile.js --cwd ./
  *
  * @package     WPPlugin
- * @version     1.2.21
+ * @version     1.2.22
  */
 
 /* jshint node: true */
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * @see http://phpenthusiast.com/blog/how-to-autoload-with-composer
  *
  * @package   	WPPlugin
- * @version     1.2.21
+ * @version     1.2.22
  */
 
   /**
