63122d848562f19cbc85dacf82ea65cc3512e1b2
Dan Smith <dev@dotherightthing.co.nz>
2018-04-27 10:10:00 +1200

    Bump version

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@
  *    gulp dist --gulpfile ./vendor/dotherightthing/wpdtrt-plugin/gulpfile.js --cwd ./
  *
  * @package     WPPlugin
- * @version     1.2.28
+ * @version     1.2.29
  */
 
 /* jshint node: true */
