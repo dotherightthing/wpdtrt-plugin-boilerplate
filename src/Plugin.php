69a3d25c9983350b821e16663825756dec0b82f5
Dan Smith <dev@dotherightthing.co.nz>
2018-05-05 00:22:40 +1200

    Merge API functions to remove redundancy

--- src/Plugin.php
+++ src/Plugin.php
@@ -785,37 +785,20 @@ if ( !class_exists( 'Plugin' ) ) {
     // API DATA
 
     /**
-     * Get the API Endpoint (URL) from which to pull data
+     * Request the data from the API
+     *  The endpoint URL is sometimes constructed from plugin specific settings fields.
+     *  We need to manually assemble the endpoint string from dynamic data.
+     *  As this data and assembly is contextual, it is done in the child plugin.
      *
-     * The endpoint URL is sometimes constructed from plugin specific settings fields.
-     * We need to manually assemble the endpoint string from dynamic data.
-     * As this data and assembly is contextual, it is done in the child plugin.
-     *
-     * A WordPress filter is used to get the correct value of $endpoint at runtime.
-     * This allows us to keep the get_api_data() code in the parent class,
-     * rather than expecting authors to overwrite get_api_data() with in the child class.
-     * Alternatively, plugin authors could simply override $this->get_api_endpoint()
-     *
-     * @return string $endpoint
+     *  A WordPress filter is used to get the correct value of $endpoint at runtime.
+     *  This allows us to keep the get_api_data() code in the parent class,
+     *  rather than expecting authors to overwrite get_api_data() with in the child class.
      *
      * @example
      *  public function wpdtrt_forms_set_api_endpoint { return $endpoint; }
      *  add_filter( 'wpdtrt_forms_set_api_endpoint', [$this, 'set_api_endpoint'] );
-     */
-    public function get_api_endpoint() {
-      // Call child plugin method:
-      // A filter is used rather than an action as actions do not return a value.
-      // A prefix prevents the filter from affecting other active instances of wpplugin.
-      $child_plugin_filter = $this->get_prefix() . '_set_api_endpoint';
-      $default_endpoint = '';
-      $endpoint = apply_filters( $child_plugin_filter, $default_endpoint );
-
-      return $endpoint;
-    }
-
-    /**
-     * Request the data from the API
      *
+     * @param       string $endpoint Used for unit testing
      * @return      object $data The body of the JSON response
      *
      * @since       0.1.0
@@ -826,12 +809,17 @@ if ( !class_exists( 'Plugin' ) ) {
      * @uses        ../../../../wp-includes/http.php
      * @see         https://developer.wordpress.org/reference/functions/wp_remote_get/
      */
-    protected function get_api_data() {
+    public function get_api_data( $endpoint = '' ) {
 
-      $endpoint = $this->get_api_endpoint();
+      // Call child plugin method:
+      // A filter is used rather than an action as actions do not return a value.
+      // A prefix prevents the filter from affecting other active instances of wpplugin.
+      $child_plugin_filter = $this->get_prefix() . '_set_api_endpoint';
+      $endpoint = apply_filters( $child_plugin_filter, $endpoint );
+
       $data = false;
 
-      if ( $endpoint ) {
+      if ( $endpoint !== '' ) {
 
         $args = array(
             'timeout' => 30, // seconds to wait for the request to complete
