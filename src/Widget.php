27fac9f53f75ab626bfbed5abe737566f6e55ab5
Dan Smith <dev@dotherightthing.co.nz>
2018-07-08 12:41:08 +1200

    Bump version

--- src/Widget.php
+++ src/Widget.php
@@ -8,7 +8,7 @@
  * @version   1.0.0
  */
 
-namespace DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_4_36;
+namespace DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_4_37;
 
 if ( ! class_exists( 'Widget' ) ) {
 
@@ -34,7 +34,7 @@ if ( ! class_exists( 'Widget' ) ) {
 		 * when it is instantiated.
 		 *
 		 * This is a public method as every plugin uses a new instance:
-		 * $wpdtrt_test_widget = new DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_4_36\Widget {}
+		 * $wpdtrt_test_widget = new DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_4_37\Widget {}
 		 *
 		 *
 		 * @param     array $options Widget options.
