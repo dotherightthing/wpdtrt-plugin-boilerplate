6944d60e896e707b9e6cff2e03543267f7601d3d
Dan Smith <dev@dotherightthing.co.nz>
2018-07-27 08:33:59 +1200

    Colourised message not working

--- gulpfile.js
+++ gulpfile.js
@@ -280,7 +280,7 @@ gulp.task("preinstall_dependencies_github", () => {
       token: get_gh_token()
     }).then( (status) => {
         log("Github API rate limit:");
-        log(`API calls remaining: ${status.core.remaining}.green/${status.core.limit}`);
+        log(`API calls remaining: ${status.core.remaining}/${status.core.limit}`);
     });
 });
 
