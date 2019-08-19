fb8c77ba3d599449b1e1baa277b18ec9fbe90259
Dan Smith <dev@dotherightthing.co.nz>
2019-08-19 23:52:36 +1200

    Bump version

--- tests/generated-plugin/src/class-wpdtrt-test-rewrite.php
+++ tests/generated-plugin/src/class-wpdtrt-test-rewrite.php
@@ -14,7 +14,7 @@
  * @since   1.0.0
  * @version 1.0.0
  */
-class WPDTRT_Test_Rewrite extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_5_12\Rewrite {
+class WPDTRT_Test_Rewrite extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_5_13\Rewrite {
 
 	/**
 	 * Supplement plugin initialisation.
--- tests/generated-plugin/src/class-wpdtrt-test-shortcode.php
+++ tests/generated-plugin/src/class-wpdtrt-test-shortcode.php
@@ -14,7 +14,7 @@
  * @since   1.0.0
  * @version 1.0.0
  */
-class WPDTRT_Test_Shortcode extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_5_12\Shortcode {
+class WPDTRT_Test_Shortcode extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_5_13\Shortcode {
 
 	/**
 	 * Supplement shortcode initialisation.
