d6d37037bbadeb90fa400fde298f60a323f1fc0b
Dan Smith <dev@dotherightthing.co.nz>
2019-08-21 02:10:44 +1200

    Try a different path for Natural Docs..

--- gulpfile.js
+++ gulpfile.js
@@ -775,7 +775,7 @@ gulp.task("docs_php", () => {
     let command = "";
 
     if ( is_travis() ) {
-        command = "mono /usr/bin/naturaldocs/NaturalDocs/NaturalDocs.exe ./config";
+        command = "mono /usr/bin/naturaldocs/NaturalDocs.exe ./config";
     } else {
         command = "mono /Applications/Natural\ Docs/NaturalDocs.exe ./config";
     }
