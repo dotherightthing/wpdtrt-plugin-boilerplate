46a3a258a1fdfa0670109f54fb81fcfc3f1591c5
Dan Smith <dev@dotherightthing.co.nz>
2019-09-04 12:34:14 +1200

    Bump version

--- src/Taxonomy.php
+++ src/Taxonomy.php
@@ -5,7 +5,7 @@
  * Plugin taxonomy class.
  */
 
-namespace DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_3;
+namespace DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_4;
 
 if ( ! class_exists( 'Taxonomy' ) ) {
 
@@ -39,7 +39,7 @@ if ( ! class_exists( 'Taxonomy' ) ) {
 		 *
 		 * Example:
 		 * --- php
-		 * $wpdtrt_test_taxonomy = new DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_3\Taxonomy {}
+		 * $wpdtrt_test_taxonomy = new DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_4\Taxonomy {}
 		 * ---
 		 *
 		 * Parameters:
