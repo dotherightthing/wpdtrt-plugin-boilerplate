1d58c69352a58a00a5a80a939c26f9a5ac1ce1de
Dan Smith <dev@dotherightthing.co.nz>
2019-09-05 23:48:44 +1200

    Bump version

--- tests/generated-plugin/src/class-wpdtrt-test-rewrite.php
+++ tests/generated-plugin/src/class-wpdtrt-test-rewrite.php
@@ -17,7 +17,7 @@
  * Since:
  *   0.7.16 - DTRT WordPress Plugin Boilerplate Generator
  */
-class WPDTRT_Test_Rewrite extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_6\Rewrite {
+class WPDTRT_Test_Rewrite extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_7\Rewrite {
 
 	/**
 	 * Constructor: __construct
