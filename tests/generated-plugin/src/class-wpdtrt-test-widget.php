e33325de5633dd1e42a62a1ff179127a9a73c67d
Dan Smith <dev@dotherightthing.co.nz>
2019-09-17 22:49:32 +1200

    Bump version

--- tests/generated-plugin/src/class-wpdtrt-test-widget.php
+++ tests/generated-plugin/src/class-wpdtrt-test-widget.php
@@ -15,7 +15,7 @@
  * Since:
  *   0.7.16 - DTRT WordPress Plugin Boilerplate Generator
  */
-class WPDTRT_Test_Widget extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_10\Widget {
+class WPDTRT_Test_Widget extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_11\Widget {
 
 	/**
 	 * Constructor: __construct
