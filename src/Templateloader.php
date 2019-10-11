6766257b68948cfee0d4332a150b5559787219e8
Dan Smith <dev@dotherightthing.co.nz>
2019-10-11 19:00:59 +1300

    Bump version

--- src/Templateloader.php
+++ src/Templateloader.php
@@ -5,7 +5,7 @@
  * Plugin template loader class.
  */
 
-namespace DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_14;
+namespace DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_15;
 
 if ( ! class_exists( 'Templateloader' ) ) {
 
@@ -40,7 +40,7 @@ if ( ! class_exists( 'Templateloader' ) ) {
 		 *
 		 * Example:
 		 * --- php
-		 * $wpdtrt_test_templateloader = new DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_14\Templateloader {}
+		 * $wpdtrt_test_templateloader = new DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_15\Templateloader {}
 		 * ---
 		 *
 		 * Parameters:
