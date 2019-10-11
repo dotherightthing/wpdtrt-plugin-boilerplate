886457b4a2c07d867a98bc05716c76eb825e1077
Dan Smith <dev@dotherightthing.co.nz>
2019-10-11 21:25:43 +1300

    Bump version

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,7 +5,7 @@
  * Plugin class.
  */
 
-namespace DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_17;
+namespace DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_18;
 
 if ( ! class_exists( 'Plugin' ) ) {
 
@@ -26,7 +26,7 @@ if ( ! class_exists( 'Plugin' ) ) {
 	class Plugin {
 
 		// https://secure.php.net/manual/en/language.oop5.constants.php.
-		const WPDTRT_PLUGIN_VERSION = '1.6.17';
+		const WPDTRT_PLUGIN_VERSION = '1.6.18';
 
 		/**
 		 * Constructor: __construct
@@ -36,7 +36,7 @@ if ( ! class_exists( 'Plugin' ) ) {
 		 * Note:
 		 * - This is a protected method as every plugin uses a sub class:
 		 * --- php
-		 * class WPDTRT_Test_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_17\Plugin {...}
+		 * class WPDTRT_Test_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_18\Plugin {...}
 		 * ---
 		 *
 		 * - A plugin-specific instance of this class is created on init:
--- tests/generated-plugin/src/class-wpdtrt-test-taxonomy.php
+++ tests/generated-plugin/src/class-wpdtrt-test-taxonomy.php
@@ -17,7 +17,7 @@
  * Since:
  *   0.7.16 - DTRT WordPress Plugin Boilerplate Generator
  */
-class WPDTRT_Test_Taxonomy extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_17\Taxonomy {
+class WPDTRT_Test_Taxonomy extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_6_18\Taxonomy {
 
 	/**
 	 * Constructor: __construct
